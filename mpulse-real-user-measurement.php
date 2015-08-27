cdb29a9dd45de0062f9b888714b9474c3ed55eaf
Nic Jansma <nic@nicj.net>
2015-08-27 15:49:46 -0400

    Fix JavaScript snippet: Add BOOMR_onload for non-NavTiming browsers, and work with W3TC

--- mpulse-real-user-measurement.php
+++ mpulse-real-user-measurement.php
@@ -88,12 +88,22 @@ function mpulse_add_rum_header() {
 ?>
 <script>
 (function(){
-  if(window.BOOMR && window.BOOMR.version){return;}
-  var dom,doc,where,iframe = document.createElement('iframe');
-  iframe.src = "javascript:false";
-  iframe.title = ""; iframe.role="presentation";
+  if (window.BOOMR && window.BOOMR.version) { return; }
+  var dom,doc,where,iframe = document.createElement("iframe"),win = window;
+
+  function boomerangSaveLoadTime(e) {
+    win.BOOMR_onload=(e && e.timeStamp) || new Date().getTime();
+  }
+  if (win.addEventListener) {
+    win.addEventListener("load", boomerangSaveLoadTime, false);
+  } else if (win.attachEvent) {
+    win.attachEvent("onload", boomerangSaveLoadTime);
+  }
+
+  iframe.src = "javascript:void(0)";
+  iframe.title = ""; iframe.role = "presentation";
   (iframe.frameElement || iframe).style.cssText = "width:0;height:0;border:0;display:none;";
-  where = document.getElementsByTagName('script')[0];
+  where = document.getElementsByTagName("script")[0];
   where.parentNode.insertBefore(iframe, where);
 
   try {
@@ -105,14 +115,14 @@ function mpulse_add_rum_header() {
   }
   doc.open()._l = function() {
     var js = this.createElement("script");
-    if(dom) this.domain = dom;
+    if (dom) { this.domain = dom; }
     js.id = "boomr-if-as";
     js.src = '//c.go-mpulse.net/boomerang/' +
-    '<?php echo get_option('mpulse_api_key');?>';
+    "<?php echo get_option('mpulse_api_key');?>";
     BOOMR_lstart=new Date().getTime();
     this.body.appendChild(js);
   };
-  doc.write('<body onload="document._l();">');
+  doc.write('<body onload="document._l();"' + String.fromCharCode(62));
   doc.close();
 })();
 </script>
