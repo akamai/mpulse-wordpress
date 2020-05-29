035d34c0e7adb7c955ed30fc1a9de567b1b3a908
Nic Jansma <nic@nicj.net>
2020-05-29 14:36:33 -0400

    mPulse Loader Snippet v14

--- mpulse-real-user-measurement.php
+++ mpulse-real-user-measurement.php
@@ -3,7 +3,7 @@
 Plugin Name: Akamai mPulse RUM
 Plugin URI: https://www.akamai.com/us/en/products/web-performance/mpulse-real-user-monitoring.jsp
 Description: What is the percieved speed of your web page & how does that impact your visitors? This is a plugin which allows you to include <a href="https://www.akamai.com/us/en/products/web-performance/mpulse-real-user-monitoring.jsp">mPulse Real User Measurement</a> in your WordPress site.
-Version: 2.5
+Version: 2.6
 Author: Akamai
 */
 
@@ -87,7 +87,7 @@ function mpulse_add_rum_header() {
 ?>
 <script>
 (function() {
-    // Boomerang Loader Snippet version 12
+    // Boomerang Loader Snippet version 14
     if (window.BOOMR && (window.BOOMR.version || window.BOOMR.snippetExecuted)) {
         return;
     }
@@ -95,12 +95,14 @@ function mpulse_add_rum_header() {
     window.BOOMR = window.BOOMR || {};
     window.BOOMR.snippetStart = new Date().getTime();
     window.BOOMR.snippetExecuted = true;
-    window.BOOMR.snippetVersion = 12;
+    window.BOOMR.snippetVersion = 14;
 
     window.BOOMR.url = "https://c.go-mpulse.net/boomerang/<?php echo get_option('mpulse_api_key'); ?>";
 
     var // document.currentScript is supported in all browsers other than IE
         where = document.currentScript || document.getElementsByTagName("script")[0],
+        // Parent element of the script we inject
+        parentNode = where.parentNode,
         // Whether or not Preload method has worked
         promoted = false,
         // How long to wait for Preload to work before falling back to iframe method
@@ -120,7 +122,7 @@ function mpulse_add_rum_header() {
         // but some naive parsers will see a missing async attribute and think we're not async
         script.async = true;
 
-        where.parentNode.appendChild(script);
+        parentNode.appendChild(script);
 
         promoted = true;
     }
@@ -151,7 +153,7 @@ function mpulse_add_rum_header() {
         if (!window.addEventListener && window.attachEvent && navigator.userAgent.match(/MSIE [67]\./)) {
             window.BOOMR.snippetMethod = "s";
 
-            bootstrap(where.parentNode, "boomr-async");
+            bootstrap(parentNode, "boomr-async");
             return;
         }
 
@@ -176,7 +178,7 @@ function mpulse_add_rum_header() {
         iframeStyle.display = "none";
 
         // Append to the end of the current block
-        where.parentNode.appendChild(iframe);
+        parentNode.appendChild(iframe);
 
         // Try to get the iframe's document object
         try {
@@ -193,14 +195,14 @@ function mpulse_add_rum_header() {
             // Set the src of the iframe to a JavaScript URL that will immediately set its document.domain to match the parent.
             // This lets us access the iframe document long enough to inject our script.
             // Our script may need to do more domain massaging later.
-            iframe.src = "javascript:var d=document.open();d.domain='" + dom + "';void(0);";
+            iframe.src = "javascript:var d=document.open();d.domain='" + dom + "';void 0;";
             win = iframe.contentWindow;
 
             doc = win.document.open();
         }
 
         if (dom) {
-            // Unsafe version for IE8 compatability. If document.domain has changed, we can't use win, but we can use doc.
+            // Unsafe version for IE8 compatibility. If document.domain has changed, we can't use win, but we can use doc.
             doc._boomrl = function() {
                 this.domain = dom;
                 bootstrap();
@@ -260,7 +262,7 @@ function mpulse_add_rum_header() {
         BOOMR_lstart = new Date().getTime();
 
         // Append our link tag
-        where.parentNode.appendChild(link);
+        parentNode.appendChild(link);
     }
     else {
         // No Preload support, use iframe loader
